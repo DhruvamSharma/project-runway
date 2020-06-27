08f78a6158ea5052ca62f204ac58478c22dcb4e4
DhruvamSharma <dhruvamssharma@gmail.com>
2020-06-27 18:03:28 +0530

    feat(app): 1. Time Picker Updated for the app

--- lib/core/notifications/local_notifications.dart
+++ lib/core/notifications/local_notifications.dart
@@ -6,6 +6,7 @@ import 'package:project_runway/core/common_colors.dart';
 import 'package:project_runway/core/constants.dart';
 import 'package:project_runway/core/injection_container.dart';
 import 'package:project_runway/core/keys/keys.dart';
+import 'package:project_runway/core/theme/theme_model.dart';
 import 'package:project_runway/features/tasks/presentation/pages/create_task/create_task_page.dart';
 import 'package:provider/provider.dart';
 
@@ -89,11 +90,15 @@ Future<void> sendNotificationForFCM(
 
 void selectTimeForNotification(BuildContext newContext, runningDate,
     Function onDateError, Function onSuccess) async {
+  final appState = Provider.of<ThemeModel>(newContext, listen: false);
   TimeOfDay timeOfDay = await showTimePicker(
       context: newContext,
       initialTime: TimeOfDay.now(),
       builder: (context, child) {
-        return child;
+        return Theme(
+          data: appState.currentTheme,
+          child: child,
+        );
       });
   if (timeOfDay != null) {
     if (runningDate.day - DateTime.now().day > 0) {
